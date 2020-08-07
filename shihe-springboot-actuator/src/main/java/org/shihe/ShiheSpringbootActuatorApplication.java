568ead9c5906a8c94a2be015a38b133c008a7543
shihe110 <github110@126.com>
2020-08-07 13:53:36 +0800

    springboot应用监控

---
+++ shihe-springboot-actuator/src/main/java/org/shihe/ShiheSpringbootActuatorApplication.java
@@ -0,0 +1,13 @@
+package org.shihe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiheSpringbootActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiheSpringbootActuatorApplication.class, args);
+    }
+
+}
